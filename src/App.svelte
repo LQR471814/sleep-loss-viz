d9455a9057251987776f0029f1d5a13113d67166
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-03-02 14:26:15 -0800

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+</script>
+
+<main>
+  <div class="text-left">
+    <h1>{import.meta.env.VITE_TARGET_NAME}'s Sleep Loss Tracker</h1>
+    <p>X amount of hours of sleep lost in total.</p>
+  </div>
+</main>
