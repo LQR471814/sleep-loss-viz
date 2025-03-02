50ee3a9bc88788bc2bd92f505d0c654bac0b9324
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-03-02 15:02:06 -0800

    update

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,30 @@
 <script lang="ts">
+  import Stat from "./Stat.svelte";
 </script>
 
-<main>
-  <div class="text-left">
-    <h1>{import.meta.env.VITE_TARGET_NAME}'s Sleep Loss Tracker</h1>
-    <p>X amount of hours of sleep lost in total.</p>
+<main class="flex">
+  <div class="flex flex-col gap-8 m-auto max-w-[600px] text-center">
+    <h1 class="text-6xl">
+      {import.meta.env.VITE_TARGET_NAME}'s Sleep Loss Tracker
+    </h1>
+
+    <div class="flex gap-8">
+      <Stat>
+        {#snippet stat()}
+          25
+        {/snippet}
+        {#snippet description()}
+          hours of sleep lost.
+        {/snippet}
+      </Stat>
+      <Stat>
+        {#snippet stat()}
+          400
+        {/snippet}
+        {#snippet description()}
+          braincells missing.
+        {/snippet}
+      </Stat>
+    </div>
   </div>
 </main>
---
+++ src/Stat.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+  const { stat, description }: { stat: Snippet; description: Snippet } =
+    $props();
+</script>
+
+<div class="flex flex-col gap-3 border border-primary p-3 aspect-square font-light">
+  <h1 class="text-6xl text-left">
+    {@render stat()}
+  </h1>
+  <div class="text-left text-2xl">
+    {@render description()}
+  </div>
+</div>
